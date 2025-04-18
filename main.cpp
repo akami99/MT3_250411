ff9aef6d7632d19cdc9bca37de0d3911bf7f1455
akami99 <k024g0059@g.neec.ac.jp>
2025-04-18 11:17:34 +0900

    MT3_00_03確認課題

--- main.cpp
+++ main.cpp
@@ -1,12 +1,70 @@
 #include <Novice.h>
+#include <assert.h>
 
-const char kWindowTitle[] = "LE2B_01_アカミネ_レン_タイトル";
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+/// <summary>
+/// ベクトル数値表示
+/// </summary>
+/// <param name="x">表示位置</param>
+/// <param name="y">表示位置</param>
+/// <param name="vector">表示するベクトル</param>
+/// <param name="label">名前</param>
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+
+/// <summary>
+/// 4x4行列数値表示
+/// </summary>
+/// <param name="x">表示位置</param>
+/// <param name="y">表示位置</param>
+/// <param name="matrix">表示する4x4行列</param>
+/// <param name="label">名前</param>
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
+
+// 1.平行移動行列
+Matrix4x4 MakeTranslationMatrix(const Vector3& translate);
+
+// 2.拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+// 3.座標変換
+Vector3 Transform(const Vector3& vector, Matrix4x4& matrix);
+
+
+const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_00_03";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+
+	Vector3 translate{ 4.1f, 2.6f, 0.8f };
+	Vector3 scale{ 1.5f, 5.2f, 7.3f };
+	Matrix4x4 trasnlateMatrix = MakeTranslationMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f, 3.8f, 1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f, 2.0f, 3.0f, 4.0f,
+		3.0f, 1.0f, 1.0f, 2.0f,
+		1.0f, 4.0f, 2.0f, 3.0f,
+		2.0f, 2.0f, 1.0f, 3.0f
+	};
+	Vector3 transformed = Transform(point, transformMatrix);
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -33,6 +91,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight, trasnlateMatrix, "trasnlateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 + kRowHeight, scaleMatrix, "scaleMatrix");
+
 		///
 		/// ↑描画処理ここまで
 		///
@@ -49,4 +111,76 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
+}
+
+// ベクトル数値表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+// 4x4行列数値表示
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+// 1.平行移動行列
+Matrix4x4 MakeTranslationMatrix(const Vector3& translate) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f;
+	result.m[1][1] = 1.0f;
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	return result;
+}
+
+// 2.拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result = {};
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1.0f;
+	return result;
+}
+
+// 3.座標変換
+Vector3 Transform(const Vector3& vector, Matrix4x4& matrix) {
+	Vector3 result = {}; // w=1がデカルト座標系であるので(x,y,z,1)のベクトルとしてmatrixとの積を取る
+	result.x =
+		vector.x * matrix.m[0][0] +
+		vector.y * matrix.m[1][0] +
+		vector.z * matrix.m[2][0] +
+		1.0f * matrix.m[3][0];
+	result.y =
+		vector.x * matrix.m[0][1] +
+		vector.y * matrix.m[1][1] +
+		vector.z * matrix.m[2][1] +
+		1.0f * matrix.m[3][1];
+	result.z =
+		vector.x * matrix.m[0][2] +
+		vector.y * matrix.m[1][2] +
+		vector.z * matrix.m[2][2] +
+		1.0f * matrix.m[3][2];
+	float w =
+		vector.x * matrix.m[0][3] +
+		vector.y * matrix.m[1][3] +
+		vector.z * matrix.m[2][3] +
+		1.0f * matrix.m[3][3];
+	assert(w != 0.0f); // ベクトルに対して基本的な操作を行う行列でwが０になることはありえない
+	result.x /= w;  // w=1がデカルト座標系であるので、w除算することで同次座標をデカルト座標に戻す
+	result.y /= w;
+	result.z /= w;
+	return result;
 }
