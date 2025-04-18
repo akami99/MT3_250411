01e2fc072fbe3fe1ccdd10f5d830de1d801bef74
akami99 <k024g0059@g.neec.ac.jp>
2025-04-18 11:23:52 +0900

    表示用関数の追加

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include <Novice.h>
-#include <math.h>
 
-
-// 画面サイズ
-const int kWindowWidth = 1280;
-const int kWindowHeight = 720;
 
 struct Vector3 {
 	float x;
@@ -12,48 +7,40 @@ struct Vector3 {
 	float z;
 };
 
-// ベクトルの表示位置
-static const int kColumnWidth = 60;
+struct Matrix4x4 {
+	float m[4][4];
+};
+
 static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+/// <summary>
+/// ベクトル数値表示
+/// </summary>
+/// <param name="x">表示位置</param>
+/// <param name="y">表示位置</param>
+/// <param name="vector">表示するベクトル</param>
+/// <param name="label">名前</param>
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 
+/// <summary>
+/// 4x4行列数値表示
+/// </summary>
+/// <param name="x">表示位置</param>
+/// <param name="y">表示位置</param>
+/// <param name="matrix">表示する4x4行列</param>
+/// <param name="label">名前</param>
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
 
-// 加算
-Vector3 Add(const Vector3& v1, const Vector3& v2);
 
-// 減算
-Vector3 Subtract(const Vector3& v1, const Vector3& v2);
 
-// スカラー倍
-Vector3 Multiply(float scalar, const Vector3& v);
-
-// 内積
-float Dot(const Vector3& v1, const Vector3& v2);
-
-// 長さ（ノルム）
-float Length(const Vector3& v);
-
-// 正規化
-Vector3 Nomalize(const Vector3& v);
-
-const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_00-01";
+const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
-
-	Vector3 v1{ 1.0f, 3.0f, -5.0f };
-	Vector3 v2{ 4.0f, -1.0f, 2.0f };
-	float k = { 4.0f };
-
-	Vector3 resultAdd = Add(v1, v2);
-	Vector3 resultSubtract = Subtract(v1, v2);
-	Vector3 resultMultiply = Multiply(k, v1);
-	float resultDot = Dot(v1, v2);
-	float resultLength = Length(v1);
-	Vector3 resultNomalize = Nomalize(v2);
+	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -80,13 +67,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, resultAdd, "  : Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, "  : Subtract");
-		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, "  : Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f  : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f  : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight * 5, resultNomalize, "  : Nomalize");
-
 		///
 		/// ↑描画処理ここまで
 		///
@@ -105,7 +85,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	return 0;
 }
 
-// ベクトルの表示位置
+// ベクトル数値表示
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
 	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
 	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
@@ -113,54 +93,13 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) 
 	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
 
-// 加算
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-	Vector3 result;
-	result.x = v1.x + v2.x;
-	result.y = v1.y + v2.y;
-	result.z = v1.z + v2.z;
-	return result;
-}
-// 減算
-Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
-	Vector3 result;
-	result.x = v1.x - v2.x;
-	result.y = v1.y - v2.y;
-	result.z = v1.z - v2.z;
-	return result;
-}
-// スカラー倍
-Vector3 Multiply(float scalar, const Vector3& v) {
-	Vector3 result;
-	result.x = scalar * v.x;
-	result.y = scalar * v.y;
-	result.z = scalar * v.z;
-	return result;
-}
-// 内積
-float Dot(const Vector3& v1, const Vector3& v2) {
-	float result;
-	result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-	return result;
-}
-// 長さ（ノルム）
-float Length(const Vector3& v) {
-	float result;
-	result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-	return result;
-}
-// 正規化
-Vector3 Nomalize(const Vector3& v) {
-	Vector3 result;
-	float length = Length(v);
-	if (length != 0) {
-		result.x = v.x / length;
-		result.y = v.y / length;
-		result.z = v.z / length;
-	} else {
-		result.x = 0;
-		result.y = 0;
-		result.z = 0;
+// 4x4行列数値表示
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
 	}
-	return result;
 }
