53c9c00b255247158d7c518eb0db690ba3eabf92
akami99 <k024g0059@g.neec.ac.jp>
2025-04-18 12:53:22 +0900

    MT3_00-05確認課題

--- main.cpp
+++ main.cpp
@@ -41,6 +41,30 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label
 /// <returns></returns>
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 
+/// <summary>
+/// X軸回転行列
+/// </summary>
+/// <param name="angle">回転角</param>
+/// <returns>回転行列</returns>
+Matrix4x4 MakeRotateXMatrix(float angle);
+
+/// <summary>
+/// Y軸回転行列
+/// </summary>
+/// <param name="angle">回転角</param>
+/// <returns>回転行列</returns>
+Matrix4x4 MakeRotateYMatrix(float angle);
+
+/// <summary>
+/// Z軸回転行列
+/// </summary>
+/// <param name="angle">回転角</param>
+/// <returns>回転行列</returns>
+Matrix4x4 MakeRotateZMatrix(float angle);
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
 
 const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_";
 
@@ -49,6 +73,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+
+	Vector3 scale{ 1.2f, 0.79f, -2.1f };
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Vector3 translate{ 2.7f, -4.15f, 1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -74,6 +105,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 
 		///
 		/// ↑描画処理ここまで
@@ -123,5 +156,64 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 			}
 		}
 	}
+	return result;
+}
+
+// X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float angle) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[1][1] = std::cos(angle);
+	result.m[1][2] = std::sin(angle);
+	result.m[2][1] = -std::sin(angle);
+	result.m[2][2] = std::cos(angle);
+	return result;
+}
+
+// Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float angle) {
+	Matrix4x4 result = {};
+	result.m[1][1] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[0][0] = std::cos(angle);
+	result.m[0][2] = -std::sin(angle);
+	result.m[2][0] = std::sin(angle);
+	result.m[2][2] = std::cos(angle);
+	return result;
+}
+// Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float angle) {
+	Matrix4x4 result = {};
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[0][0] = std::cos(angle);
+	result.m[0][1] = std::sin(angle);
+	result.m[1][0] = -std::sin(angle);
+	result.m[1][1] = std::cos(angle);
+	return result;
+}
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 result = {};
+	// X,Y,Z軸の回転をまとめる
+	Matrix4x4 rotateXYZ =
+		Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
+
+	result.m[0][0] = scale.x * rotateXYZ.m[0][0];
+	result.m[0][1] = scale.x * rotateXYZ.m[0][1];
+	result.m[0][2] = scale.x * rotateXYZ.m[0][2];
+	result.m[1][0] = scale.y * rotateXYZ.m[1][0];
+	result.m[1][1] = scale.y * rotateXYZ.m[1][1];
+	result.m[1][2] = scale.y * rotateXYZ.m[1][2];
+	result.m[2][0] = scale.z * rotateXYZ.m[2][0];
+	result.m[2][1] = scale.z * rotateXYZ.m[2][1];
+	result.m[2][2] = scale.z * rotateXYZ.m[2][2];
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1.0f;
+
 	return result;
 }
