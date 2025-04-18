bc4e34873c9b61df1f2b09fef4d9fba7cb018435
akami99 <k024g0059@g.neec.ac.jp>
2025-04-18 11:55:12 +0900

    MT3_00-04確認課題

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include <cmath>
 
 
 struct Vector3 {
@@ -32,15 +33,40 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 /// <param name="label">名前</param>
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
 
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1">掛ける行列1</param>
+/// <param name="m2">掛ける行列2</param>
+/// <returns></returns>
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+// 1.X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float angle);
+
+// 2.Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float angle);
+
+// 3.Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float angle);
 
 
-const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_";
+
+const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_00-04";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -66,6 +92,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "RotateXMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix, "RotateYMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix, "RotateZMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "RotateXYZMatrix");
 
 		///
 		/// ↑描画処理ここまで
@@ -102,4 +133,53 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label
 				x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f", matrix.m[row][column]);
 		}
 	}
+}
+
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result.m[i][j] = 0;
+			for (int k = 0; k < 4; k++) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+	return result;
+}
+
+// 1.X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float angle) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[1][1] = std::cos(angle);
+	result.m[1][2] = std::sin(angle);
+	result.m[2][1] = -std::sin(angle);
+	result.m[2][2] = std::cos(angle);
+	return result;
+}
+
+// 2.Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float angle) {
+	Matrix4x4 result = {};
+	result.m[1][1] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[0][0] = std::cos(angle);
+	result.m[0][2] = -std::sin(angle);
+	result.m[2][0] = std::sin(angle);
+	result.m[2][2] = std::cos(angle);
+	return result;
+}
+// 3.Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float angle) {
+	Matrix4x4 result = {};
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[0][0] = std::cos(angle);
+	result.m[0][1] = std::sin(angle);
+	result.m[1][0] = -std::sin(angle);
+	result.m[1][1] = std::cos(angle);
+	return result;
 }
