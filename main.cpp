e2359d0c0abc400785ae1e81da48535765a56e6c
akami99 <k024g0059@g.neec.ac.jp>
2025-04-25 14:45:44 +0900

    MT3_01-01ポリゴンを描こう

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <cassert>
 
 
+static const int kWindowWidth = 1280;
+static const int kWindowHeight = 720;
+
 struct Vector3 {
 	float x;
 	float y;
@@ -50,7 +53,6 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 /// <returns>変換させた座標</returns>
 Vector3 Transform(const Vector3& vector, Matrix4x4& matrix);
 
-
 /// <summary>
 /// X軸回転行列
 /// </summary>
@@ -72,7 +74,6 @@ Matrix4x4 MakeRotateYMatrix(float angle);
 /// <returns>Z軸回転行列</returns>
 Matrix4x4 MakeRotateZMatrix(float angle);
 
-
 /// <summary>
 /// 3次元アフィン変換行列
 /// </summary>
@@ -81,7 +82,6 @@ Matrix4x4 MakeRotateZMatrix(float angle);
 /// <param name="translate">移動量</param>
 /// <returns>3次元アフィン行列</returns>
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
-
 
 /// <summary>
 /// 透視投影行列
@@ -112,18 +112,42 @@ Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, f
 /// <returns>逆行列</returns>
 Matrix4x4 Inverse(const Matrix4x4& m);
 
+// クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
 
-const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_";
+
+const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_01-01";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+
+	Vector3 v1{ 1.2f, -3.9f, 2.5f };
+	Vector3 v2{ 2.8f, 0.4f, -1.3f };
+	Vector3 cross = Cross(v1, v2);
+
+	Vector3 rotate{};
+	Vector3 translate{};
+	Vector3 cameraPosition{ 0.0f, 0.0f, -5.0f };
+	const Vector3 kLocalVertices[3]{ {0.0f, 0.5f, 0.0f}, {-0.5f, -0.5f, 0.0f}, {0.5f, -0.5f, 0.0f} };
+
+	// 各種行列の宣言
+	Matrix4x4 worldMatrix{};
+	Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, cameraPosition);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, static_cast<float>(kWindowWidth) / static_cast<float>(kWindowHeight), 0.1f, 100.0f);
+	Matrix4x4 worldViewProjectionMatrix{};
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, static_cast<float>(kWindowWidth), static_cast<float>(kWindowHeight), 0.0f, 1.0f);
+	Vector3 screenVertices[3];
+	Vector3 ndcVertex{};
+
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -138,6 +162,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		// WSキーで前後に、ADキーで左右に三角形を動かす。Y軸回転をさせる。という処理をここに書く、translateとrotateの値を変更すれば良い。
+		if (keys[DIK_W]) {
+			translate.z += 0.1f;
+		}
+		if (keys[DIK_S]) {
+			translate.z += -0.1f;
+		}
+		if (keys[DIK_A]) {
+			translate.x += -0.1f;
+		}
+		if (keys[DIK_D]) {
+			translate.x += 0.1f;
+		}
+
+		// Y軸回転をさせる
+		rotate.y += 0.05f;
+
+		// 各種行列の計算
+		worldMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate, translate);
+		worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		for (uint32_t i = 0; i < 3; ++i) {
+			ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
+			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -145,6 +194,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, cross, "Cross");
+
+		// 描画
+		Novice::DrawTriangle(
+			static_cast<int>(screenVertices[0].x), static_cast<int>(screenVertices[0].y), static_cast<int>(screenVertices[1].x), static_cast<int>(screenVertices[1].y),
+			static_cast<int>(screenVertices[2].x), static_cast<int>(screenVertices[2].y), RED, kFillModeSolid
+		);
 
 		///
 		/// ↑描画処理ここまで
@@ -364,5 +421,14 @@ Matrix4x4 Inverse(const Matrix4x4& m) {
 	result.m[3][3] = (m.m[0][0] * m.m[1][1] * m.m[2][2] + m.m[0][1] * m.m[1][2] * m.m[2][0] + m.m[0][2] * m.m[1][0] * m.m[2][1]
 		- m.m[0][2] * m.m[1][1] * m.m[2][0] - m.m[0][1] * m.m[1][0] * m.m[2][2] - m.m[0][0] * m.m[1][2] * m.m[2][1]) / determinant;
 
+	return result;
+}
+
+// クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2) {
+	Vector3 result{};
+	result.x = (v1.y * v2.z) - (v1.z * v2.y);
+	result.y = (v1.z * v2.x) - (v1.x * v2.z);
+	result.z = (v1.x * v2.y) - (v1.y * v2.x);
 	return result;
 }
