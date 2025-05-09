4cc7840ca5972c4b1b01668a27f073ac46f38a41
akami99 <k024g0059@g.neec.ac.jp>
2025-05-09 16:22:51 +0900

    デバッグカメラとグリッド、球体を描画するための関数を追加

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <Novice.h>
 #include <cmath>
 #include <cassert>
+#include <imgui.h>
+#include <numbers>
 
 
-static const int kWindownWidth = 1280;
+static const int kWindowWidth = 1280;
 static const int kWindowHeight = 720;
 
 struct Vector3 {
@@ -14,6 +16,11 @@ struct Vector3 {
 
 struct Matrix4x4 {
 	float m[4][4];
+};
+
+struct Sphere {
+	Vector3 center; //!< 中心点
+	float radius;   //!< 半径
 };
 
 static const int kRowHeight = 20;
@@ -38,6 +45,14 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
 
 /// <summary>
+/// 加算
+/// </summary>
+/// <param name="v1">行列1</param>
+/// <param name="v2">行列2</param>
+/// <returns>行列の和</returns>
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+
+/// <summary>
 /// 4x4行列の積
 /// </summary>
 /// <param name="m1">掛ける行列1</param>
@@ -51,7 +66,7 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 /// <param name="vector">変換したいベクトル</param>
 /// <param name="matrix">変換させる行列</param>
 /// <returns>変換させた座標</returns>
-Vector3 Transform(const Vector3& vector, Matrix4x4& matrix);
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
 
 /// <summary>
 /// X軸回転行列
@@ -106,6 +121,31 @@ Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip
 Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
 
 /// <summary>
+/// 球面をデカルト座標に変換
+/// </summary>
+/// <param name="radius">半径</param>
+/// <param name="lat">緯度</param>
+/// <param name="lon">経度</param>
+/// <returns>デカルト座標</returns>
+Vector3 SphericalToCartesian(float radius, float lat, float lon);
+
+/// <summary>
+/// 球を描画する関数
+/// </summary>
+/// <param name="sphere">球</param>
+/// <param name="viewProjectionMatrix">ビュー射影行列</param>
+/// <param name="viewportMatrix">ビューポート行列</param>
+/// <param name="color">色</param>
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
+
+/// <summary>
+/// グリッドを描画する関数
+/// </summary>
+/// <param name="viewProjectionMatrix">ビュー射影行列</param>
+/// <param name="viewportMatrix">ビューポート行列</param>
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
+
+/// <summary>
 /// 逆行列
 /// </summary>
 /// <param name="m">逆行列に変換する行列</param>
@@ -119,7 +159,20 @@ const char kWindowTitle[] = "LE2B_01_アカミネ_レン_MT3_";
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, kWindownWidth, kWindowHeight);
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	
+
+	// カメラの設定
+	Vector3 cameraTranslate{ 0.0f, 1.9f, -6.49f };
+	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
+
+	Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+	Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, static_cast<float>(kWindowWidth) / static_cast<float>(kWindowHeight), 0.1f, 100.0f);
+	Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, static_cast<float>(kWindowWidth), static_cast<float>(kWindowHeight), 0.0f, 1.0f);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -138,6 +191,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+
+
+#ifdef _DEBUG
+		// カメラの更新
+		cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
+		viewMatrix = Inverse(cameraMatrix);
+		projectionMatrix = MakePerspectiveFovMatrix(0.45f, static_cast<float>(kWindowWidth) / static_cast<float>(kWindowHeight), 0.1f, 100.0f);
+		viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+
+#endif // _DEBUG
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -145,6 +209,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+
+
+
+#ifdef _DEBUG
+		// デバッグウィンドウ
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::End();
+
+#endif // _DEBUG
 
 		///
 		/// ↑描画処理ここまで
@@ -183,6 +261,15 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label
 	}
 }
 
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return result;
+}
+
 // 4x4行列の積
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 result;
@@ -198,7 +285,7 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 }
 
 // 座標変換
-Vector3 Transform(const Vector3& vector, Matrix4x4& matrix) {
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 	Vector3 result = {}; // w=1がデカルト座標系であるので(x,y,z,1)のベクトルとしてmatrixとの積を取る
 	result.x =
 		vector.x * matrix.m[0][0] +
@@ -310,6 +397,98 @@ Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, f
 	result.m[3][2] = minDepth;
 	result.m[3][3] = 1.0f;
 	return result;
+}
+
+// 球面をデカルト座標に変換
+Vector3 SphericalToCartesian(float radius, float lat, float lon) {
+	float x = radius * cosf(lat) * cosf(lon);
+	float y = radius * sinf(lat);
+	float z = radius * cosf(lat) * sinf(lon);
+	return { x, y, z };
+}
+
+// 球を描画する関数
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+	const uint32_t kSubdivision = 12;   // 分割数
+	const float kLonEvery = static_cast<float>(std::numbers::pi * 2 / kSubdivision);      // 経度分割1つ分の角度(φ)
+	const float kLatEvery = static_cast<float>(std::numbers::pi / kSubdivision);      // 緯度分割1つ分の角度(θ)
+	// 緯度の方向に分割 -π/2 ~ π/2
+	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex) {
+		float lat = static_cast<float>(-std::numbers::pi / 2.0f + kLatEvery * latIndex);// 現在の緯度
+		float latNext = lat + kLatEvery;
+
+		// 経度の方向に分割 0 ~ π/2
+		for (uint32_t lonIndex = 0; lonIndex < kSubdivision; ++lonIndex) {
+			float lon = lonIndex * kLonEvery;// 現在の経度
+			float lonNext = lon + kLonEvery;
+
+			// world座標系でa,b,cを求める
+			Vector3 a = Add(SphericalToCartesian(sphere.radius, lat, lon), sphere.center);
+			Vector3 b = Add(SphericalToCartesian(sphere.radius, latNext, lon), sphere.center);
+			Vector3 c = Add(SphericalToCartesian(sphere.radius, lat, lonNext), sphere.center);
+
+			// a,b,cをScreen座標系まで変換...
+			Vector3 screenA = Transform(Transform(a, viewProjectionMatrix), viewportMatrix);
+			Vector3 screenB = Transform(Transform(b, viewProjectionMatrix), viewportMatrix);
+			Vector3 screenC = Transform(Transform(c, viewProjectionMatrix), viewportMatrix);
+
+			// ab,bc->acで線を引く
+			Novice::DrawLine(static_cast<int>(screenA.x), static_cast<int>(screenA.y),
+				static_cast<int>(screenB.x), static_cast<int>(screenB.y), color);
+			Novice::DrawLine(static_cast<int>(screenA.x), static_cast<int>(screenA.y),
+				static_cast<int>(screenC.x), static_cast<int>(screenC.y), color); // a→c
+		}
+	}
+}
+
+// グリッドを描画する関数
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
+	const float kGridHalfWidth = 2.0f;                                                  // Gridの半分の幅
+	const uint32_t kSubdivision = 10;                                                   // 分割数
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / static_cast<float>(kSubdivision);// 1つ分の長さ
+	// 奥から手前への線を徐々に引いていく
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex) {
+		// 上の情報を使ってワ－ルド座標系上の始点と終点を求める
+		float x = -kGridHalfWidth + kGridEvery * static_cast<float>(xIndex);
+
+		Vector3 start = { x, 0.0f, -kGridHalfWidth };
+		Vector3 end = { x, 0.0f, kGridHalfWidth };
+
+		// スクリーン座標系まで変換を書ける
+		Vector3 screenStart = Transform(Transform(start, viewProjectionMatrix), viewportMatrix);
+		Vector3 screenEnd = Transform(Transform(end, viewProjectionMatrix), viewportMatrix);
+
+		// 変換した座標を使って表示。色は薄い灰色(0xAAAAAAFF)、原点は黒ぐらいが良いが、何でも良い
+		if (xIndex == kSubdivision / 2) {
+			Novice::DrawLine(static_cast<int>(screenStart.x), static_cast<int>(screenStart.y),
+				static_cast<int>(screenEnd.x), static_cast<int>(screenEnd.y), 0x000000FF);
+		} else {
+			Novice::DrawLine(static_cast<int>(screenStart.x), static_cast<int>(screenStart.y),
+				static_cast<int>(screenEnd.x), static_cast<int>(screenEnd.y), 0xAAAAAAFF);
+		}
+	}
+	// 左から右も同じように順々にひいていく
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex) {
+		// 奥から手前が左右に変わるだけ
+		// 上の情報を使ってワ－ルド座標系上の始点と終点を求める
+		float z = -kGridHalfWidth + kGridEvery * static_cast<float>(zIndex);
+
+		Vector3 start = { -kGridHalfWidth, 0.0f, z };
+		Vector3 end = { kGridHalfWidth, 0.0f, z };
+
+		// スクリーン座標系まで変換を書ける
+		Vector3 screenStart = Transform(Transform(start, viewProjectionMatrix), viewportMatrix);
+		Vector3 screenEnd = Transform(Transform(end, viewProjectionMatrix), viewportMatrix);
+
+		// 変換した座標を使って表示。色は薄い灰色(0xAAAAAAFF)、原点は黒ぐらいが良いが、何でも良い
+		if (zIndex == kSubdivision / 2) {
+			Novice::DrawLine(static_cast<int>(screenStart.x), static_cast<int>(screenStart.y),
+				static_cast<int>(screenEnd.x), static_cast<int>(screenEnd.y), 0x000000FF);
+		} else {
+			Novice::DrawLine(static_cast<int>(screenStart.x), static_cast<int>(screenStart.y),
+				static_cast<int>(screenEnd.x), static_cast<int>(screenEnd.y), 0xAAAAAAFF);
+		}
+	}
 }
 
 // 逆行列
