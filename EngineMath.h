58763adfa059137a0dbbd6419ef55bb850eff3cd
akami99 <k024g0059@g.neec.ac.jp>
2025-07-18 10:06:57 +0900

    ばねの構造体の追加

--- EngineMath.h
+++ EngineMath.h
@@ -49,4 +49,21 @@ struct Plane {
 	float distance; //!< 距離
 };
 
+struct Spring {
+	// アンカー。固定された端の位置
+	Vector3 anchor;
+	float naturalLength;      // 自然長
+	float stiffness;          // 剛性。ばね定数k
+	float dampingCoefficient; // 減衰。減衰係数c
+};
+
+struct Ball {
+	Vector3 position;         // ボールの位置
+	Vector3 velocity;         // ボールの速度
+	Vector3 acceleration;     // ボールの加速度
+	float mass;               // ボールの質量
+	float radius;             // ボールの半径
+	unsigned int color;       // ボールの色
+};
+
 #endif // ENGINE_MATH_H
